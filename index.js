26fa7903642f394d6db99622b0cb7b1036eac8f3
kapysta <kapysta000017@gmail.com>
2024-06-09 15:16:22 +0300

    feat: fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const element = document.querySelector("button")
 
 element.addEventListener("click", function (event) {
-  fetch("http://45.89.189.42/api/todos/1", {
+  fetch("http://45.89.189.42/todos/1", {
+    method: "GET",
     headers: {
       Accept: "application/json",
     },
